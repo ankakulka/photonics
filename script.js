b22489d114d105736c5e37c1a1845b8ce6da3f01
ankakulczycka@gmail.com <ankakulczycka@gmail.com>
2022-02-03 16:22:07 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
+
+
+
+
+
+// NUMBER OF PULSES
+
+
+
+// ALPHABETICAL NAV LIST
+$('#demoSix').listnav({
+    filterSelector: '.last-name',
+    includeNums: false,
+    removeDisabled: true,
+    allText: 'Complete Cast of Star Trek TNG'
+});
+
+
+$('#demoOne').listnav();
+
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "My First dataset",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
